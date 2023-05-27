576f5f084e5fe9105e29a13634862f6f7ac8bd2e
Skeppar <alvin.skeppar@gmail.com>
2023-05-28 00:43:24 +0200

    Made some modifications. Now it starts from the lowest number instead of highest (startLength and maxLength) and also tells you how many attempts it took to crack if it didn't find the password in the password.txt file.

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Start");
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a password you want to test:");
+        String enteredPassword = sc.nextLine();
 
         String filePath = "src/passwords.txt"; // Path to the text file with passwords
         String[] commonPasswords = readPasswordsFromFile(filePath);
-        AlphabetPrinter.printCombinations(4, 10, commonPasswords, "glamour");
+        AlphabetPrinter.printCombinations(4, 10, commonPasswords, enteredPassword); // This is the password length, most passwords are over 6 characters in length, but I used 4 just as a test.
     }
 
     private static String[] readPasswordsFromFile(String filePath) {
@@ -36,56 +38,54 @@ public class Main {
         private static final String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
         public static void printCombinations(int startLength, int maxLength, String[] commonPasswords, String targetPassword) {
-
             long startTime = System.currentTimeMillis();
 
             // Print passwords from the file
             boolean targetFound = false;
+            int count = 1;
             for (String password : commonPasswords) {
                 if (password.length() >= startLength && password.length() <= maxLength) {
-                    System.out.println(password);
+                    //System.out.println("Found common password from file: " + password);
                     if (password.equals(targetPassword)) {
                         long endTime = System.currentTimeMillis();
                         long elapsedTime = endTime - startTime;
-                        System.out.println("Password found in common password file.\nPassword: " + password);
-                        System.out.println("Time taken to find the password: " + elapsedTime + " milliseconds");
+                        System.out.println("Password reached: " + password);
+                        System.out.println("Time taken: " + elapsedTime + " milliseconds");
+                        System.out.println("Your password is number " + count + " amongst the most common passwords in the world");
                         targetFound = true;
                         break;
                     }
+                    count++;
                 }
             }
 
             // Generate combinations if target password not found
             if (!targetFound) {
-                generateCombinations("", startLength, maxLength, startTime, targetPassword);
-            }
-        }
-
-        private static void generateCombinations(String combination, int length, int maxLength, long startTime, String targetPassword) {
-
-            if (combination.length() == length) {
-                System.out.println("Current combination: " + combination);
-                return;
-            }
-
-            for (int i = 0; i < CHARS.length(); i++) {
-                String newCombination = combination + CHARS.charAt(i);
-                generateCombinations(newCombination, length, maxLength, startTime, targetPassword);
-            }
-
-            if (length < maxLength) {
-                generateCombinations(combination, length + 1, maxLength, startTime, targetPassword);
-            }
-        }
-
-
-        private static boolean containsPassword(String combination, String[] commonPasswords) {
-            for (String password : commonPasswords) {
-                if (password.equals(combination)) {
-                    return true;
+                for (int length = startLength; length <= maxLength; length++) {
+                    generateCombinations("", length, startTime, targetPassword, count);
                 }
             }
-            return false;
+        }
+
+        private static void generateCombinations(String combination, int length, long startTime, String targetPassword, int count) {
+            if (combination.length() == length) {
+                //System.out.println("Current combination: " + combination);
+                if (combination.equals(targetPassword)) {
+                    long endTime = System.currentTimeMillis();
+                    long elapsedTime = endTime - startTime;
+                    System.out.println("Password reached: " + combination);
+                    System.out.println("It took " + count + " of attempts to break your password");
+                    System.out.println("Time taken: " + elapsedTime + " milliseconds");
+                    System.exit(0);
+                }
+            }
+
+            if (combination.length() < length) {
+                for (int i = 0; i < CHARS.length(); i++) {
+                    String newCombination = combination + CHARS.charAt(i);
+                    generateCombinations(newCombination, length, startTime, targetPassword, count);
+                }
+            }
         }
     }
 }
